a41db9998295c6f2958e22a58b2dcc14b53401fa
Cesar Moreno <cesar200526@gmail.com>
2016-03-20 10:53:54 -0500

    Agregando nombre del proyecto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Proyecto Dashboard</div>
+                <div class="title">NosVenden</div>
             </div>
         </div>
     </body>

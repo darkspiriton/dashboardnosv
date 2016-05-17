72c24dad1c6b6be35b3a5b047fb546a44a671e36
Cesar Moreno <cesar200526@gmail.com>
2016-05-17 09:33:56 -0500

    Se esta agregando configuracion de Asistencia

--- app/Http/Controllers/AssistController.php
+++ app/Http/Controllers/AssistController.php
@@ -2,9 +2,13 @@
 
 namespace Dashboard\Http\Controllers;
 
+use Dashboard\Models\Planilla\Assist;
+use Dashboard\Models\Planilla\Employe;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 use Dashboard\Http\Requests;
+use Mockery\CountValidator\Exception;
 
 class AssistController extends Controller
 {
@@ -36,7 +40,123 @@ class AssistController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'employe_id' => 'required',
+            'start_time' => 'required',
+            'end_time' => 'required',
+            'date' => 'required',
+//            'start_time_launch' => 'required',
+//            'end_time_launch' => 'required',
+//            'type' => 'required',
+//            'justification' => 'required',
+        ];
+
+        try{
+            $validator = \Validator::make($request->all(), $rules);
+            if($validator->fails()){
+                return response()->json(['message'=>'No posee todo los campos necesarios para crear una asistencia'],401);
+            }
+            $date = Carbon::Parse($request->input('date'));
+            $date->setTimezone('-5');
+            dd($date->dayOfWeek);
+
+            $employe = Employe::find($request->input('employe_id'));
+            $emplote = 
+            
+            
+
+
+
+            if($request->input('start_time')!=null){
+                $start_day = Carbon::Parse($request->input('start_time'));
+                $start_day->setTimezone('-5');
+                if($request->input('end_time')!=null){
+                    $end_day = Carbon::Parse($request->input('end_time'));
+                    $end_day->setTimezone('-5');
+                    if($request->input('end_time_launch')!=null){
+                        $end_time_launch= Carbon::Parse($request->input('end_time_launch'));
+                        $end_time_launch->setTimezone('-5');
+                        if($request->input('start_time_launch')!=null){
+                            $start_time_launch = Carbon::Parse($request->input('start_time_launch'));
+                            $start_time_launch->setTimezone('-5');
+                            //Calculo de horas 1234
+                            $this->calculo1234($start_day,$start_time_launch,$end_time_launch,$end_day);
+                        }
+                    }
+                }elseif($request->input('end_time_launch')!=null) {
+                    $end_day = Carbon::Parse($request->input('end_time_launch'));
+                    $end_day->setTimezone('-5');
+                    if($request->input('start_time_launch')!=null){
+                        $start_time_launch = Carbon::Parse($request->input('start_time_launch'));
+                        $start_time_launch->setTimezone('-5');
+                        //Calculo de horas 123
+                        $this->calculo123($start_day,$start_time_launch,$end_day);
+                    }
+                }elseif($request->input('start_time_launch')!=null){
+                    $end_day = Carbon::Parse($request->input('start_time_launch'));
+                    $end_day->setTimezone('-5');
+                    //Calculo de horas 12
+                    $this->calculo12($start_day,$end_day);
+                }
+
+            }elseif($request->input('start_time_launch')!=null){
+                $start_day = Carbon::Parse($request->input('start_time_launch'));
+                $start_day->setTimezone('-5');
+                if($request->input('end_time')!=null){
+                    $end_day = Carbon::Parse($request->input('end_time'));
+                    $end_day->setTimezone('-5');
+                    if($request->input('end_time_launch')!=null){
+                        $end_time_launch = Carbon::Parse($request->input('end_time_launch'));
+                        $end_time_launch->setTimezone('-5');
+                        //Calculo de horas 234
+                        $this->calculo234($start_day,$end_time_launch,$end_day);
+
+                    }
+
+                }elseif($request->input('end_time_launch')!= null){
+                    $end_day = Carbon::Parse($request->input('end_time_launch'));
+                    $end_day->setTimezone('-5');
+                    //Calculo de horas 23
+                    $this->calculo23($start_day,$end_day);
+                }
+
+            }elseif($request->input('end_time_launch')!=null){
+                $start_day = Carbon::Parse($request->input('end_time_launch'));
+                $start_day->setTimezone('-5');
+                if($request->input('end_time')!=null){
+                    $end_day = Carbon::Parse($request->input('end_time'));
+                    $end_day->setTimezone('-5');
+                    //Calculo de horas 34
+                    $this->calculo34($start_day,$end_day);
+                }
+
+            }
+
+            dd($start_day->diffInMinutes($end_day));
+
+            
+
+            $assist = new Assist();
+            $assist->employe_id=$employe->id;
+            $assist->start_time=$start_day->toTimeString();
+            $assist->end_time=$end_day->toTimeString();
+//            $assist->type=$request->input('type');
+//            $assist->type=$request->input('justification');
+            $assist->save();
+
+
+
+            dd($assist->created_at);
+
+
+
+            //Funcion que procese la asistencia y realice el calculo si fuera necesario.
+
+
+        }catch(Exception $e){
+            return response()->json(['message' => 'Ocurrio un error al agregar la asistencia'],500);
+        }
+
     }
 
     /**
@@ -82,5 +202,29 @@ class AssistController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    private function calculo1234($inicio,$inicioAlmuerzo,$finAlmuerzo,$fin){
+
+    }
+
+    private function calculo123($inicio,$inicioAlmuerzo,$fin){
+
+    }
+
+    private function calculo12($inicio,$inicioAlmuerzo){
+
+    }
+
+    private function calculo234($fin,$inicioAlmuerzo,$finAlmuerzo){
+
+    }
+
+    private function calculo23($inicioAlmuerzo,$finAlmuerzo){
+
+    }
+
+    private function calculo34($finAlmuerzo,$fin){
+
     }
 }

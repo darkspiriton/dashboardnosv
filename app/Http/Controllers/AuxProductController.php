dcd850991ff704bf076ae0830f9868f1ac92ae41
Luis Vasquez <luizhito.lp.4ever@gmail.com>
2016-05-23 18:34:53 -0500

    Correcion de nombre de columna de proveedor

--- app/Http/Controllers/AuxProductController.php
+++ app/Http/Controllers/AuxProductController.php
@@ -27,7 +27,7 @@ class AuxProductController extends Controller
     public function index()
     {
         $products = DB::table('auxproducts as p')
-                        ->select(DB::raw('DATE_FORMAT(p.created_at,\'%d-%m-%Y\') as date'),'p.id','p.cod','p.name','c.name as color','c.name as size','p.name as provider',DB::raw('GROUP_CONCAT(t.name SEPARATOR \' - \') as types'))
+                        ->select(DB::raw('DATE_FORMAT(p.created_at,\'%d-%m-%Y\') as date'),'p.id','p.cod','p.name','c.name as color','c.name as size','pv.name as provider',DB::raw('GROUP_CONCAT(t.name SEPARATOR \' - \') as types'))
                         ->join('types_auxproducts as tp','tp.product_id','=','p.id')
                         ->join('types as t','t.id','=','tp.type_id')
                         ->join('colors as c','c.id','=','p.color_id')

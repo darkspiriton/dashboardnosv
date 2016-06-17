ed63103d65574dae07b7beb47f087100ba8aaca5
Cesar Moreno <cesar200526@gmail.com>
2016-06-17 18:15:24 -0500

    Se agrego prueba de creacion de publicacion 6

--- app/Http/Controllers/PublicityController.php
+++ app/Http/Controllers/PublicityController.php
@@ -149,15 +149,16 @@ class PublicityController extends Controller
         $process->status = 1;
         $process->date_finish=$date_finish->copy()->toDateTimeString();
 
+        $id = $process->type_process_id;
         $publicity_process = new Process();
         $publicity_process->publicity_id = $publicity->id;
         $publicity_process->date = $date_finish->toDateTimeString();
         $publicity_process->date_finish="0000-00-00 00:00:00";
-        $publicity_process->type_process_id = $process->type_process_id->replicate() +1;
+        $publicity_process->type_process_id = $id +1;
         $publicity_process->status = 0;
 
+        return response()->json(['process' => $process, 'publi' => $publicity_process],200);
         $process->save();
-
         $publicity_process->save();
 
         return response()->json(['message' => 'Se registro avance de publicidad'], 200);

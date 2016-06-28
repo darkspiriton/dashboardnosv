0e214d3e3d5f8fc180af8fcba893a36dfa0cc11a
Luis vasquez <luizhito.lp.4ever@gmail.com>
2016-06-28 19:11:12 +0000

    AuxMovementOutFitController.php editado a través de la web en Bitbucket

--- app/Http/Controllers/AuxMovementOutFitController.php
+++ app/Http/Controllers/AuxMovementOutFitController.php
@@ -14,7 +14,6 @@ class AuxMovementOutFitController extends Controller
     public function __construct()
     {
         $this->middleware('auth:GOD,ADM,JVE');
-        $this->middleware('auth:GOD,ADM', ['only' => ['store','update']]);
     }
 
     /**

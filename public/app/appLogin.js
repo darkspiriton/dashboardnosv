0942715f3ab071fc40f35bf8c1d9e5996c52ac2a
Luis Vasquez <luizhito.lp.4ever@gmail.com>
2016-07-27 12:58:15 -0500

    Correcion de modulo app angular para su compilacion

--- public/app/appLogin.js
+++ public/app/appLogin.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-angular.module('loginApp', ['ngResource', 'ngMessages', 'ngAnimate', 'toastr', 'ui.router', 'satellizer']).config(function ($stateProvider, $urlRouterProvider, $authProvider) {
+angular.module('loginApp', ['ngResource', 'ngMessages', 'ngAnimate', 'toastr', 'ui.router', 'satellizer']).config(["$stateProvider", "$urlRouterProvider", "$authProvider", function ($stateProvider, $urlRouterProvider, $authProvider) {
 
     $stateProvider.state('homeLogin', {
         url: '/login',
@@ -13,7 +13,7 @@ angular.module('loginApp', ['ngResource', 'ngMessages', 'ngAnimate', 'toastr', '
 
     $authProvider.tokenName = "token";
     $authProvider.tokenPrefix = "DB_NV";
-});
+}]);
 
 },{}]},{},[1]);
 

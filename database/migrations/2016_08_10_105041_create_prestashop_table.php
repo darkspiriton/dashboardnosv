cff9b4b10ce42d71f66907a2c85843aa60743c06
Cesar Moreno <cesar200526@gmail.com>
2016-08-10 11:14:47 -0500

    Se agrego migracion de compras prestashop

---
+++ database/migrations/2016_08_10_105041_create_prestashop_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrestashopTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_prestashop',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+        });
+
+        Schema::create('requests_prestashop',function(Blueprint $table){
+            $table->increments('id');                        
+            $table->boolean('status');
+            $table->integer('total_price');
+            $table->integer('user_id')->unsigned();            
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users_prestashop');
+        });
+
+        Schema:: create('products_prestahop', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->string('url_image');
+            $table->string('url_prodcut');
+            $table->integer('stock');
+            $table->float('price');
+            $table->integer('cant');
+            $table->integer('request_id')->unsigned();
+
+            $table->foreign('request_id')->references('id')->on('products_prestahop');
+        });
+
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_prestashop');
+        Schema::dropIfExists('requests_prestashop');
+        Schema::dropIfExists('products_prestahop');
+    }
+}

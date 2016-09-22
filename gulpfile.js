12316f4bfec9bb5d468c2ba4749fa30a7dad7a2c
Cesar Moreno <cesar200526@gmail.com>
2016-09-22 15:20:27 -0500

    Se agrego opcion de kardex coordinador

--- gulpfile.js
+++ gulpfile.js
@@ -174,6 +174,7 @@ elixir(function(mix) {
     mix.browserify([
         "app/controllers/auxStockCtrl.js",
         "app/controllers/auxStockVENCtrl.js",
+        "app/controllers/auxMovement2_JVECtrl.js",
         "app/controllers/outFitCtrl.js",
         "app/controllers/auxMovementOutFitCtrl.js",
         "app/controllers/auxMovementOutFit2Ctrl.js",
